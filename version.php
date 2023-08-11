671ffed0a63c5125ff87af4ac903fd9b81049165
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-08-11 14:26:50 +0100

    v0.1 b4

--- version.php
+++ version.php
@@ -11,5 +11,5 @@ $plugin->component = 'local_modulecompletion';
 $plugin->version = 7;
 $plugin->requires = 2016052314;
 $plugin->dependencies = [
-    'local_trainingplan' => 20
+    'local_trainingplan' => 23
 ];
